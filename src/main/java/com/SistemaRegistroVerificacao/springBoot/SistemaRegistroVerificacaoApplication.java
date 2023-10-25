a8000d38033b4ef22751ef452a7a2306367b3ff8
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-10-25 15:13:31 -0300

    SpringBooot

---
+++ src/main/java/com/SistemaRegistroVerificacao/springBoot/SistemaRegistroVerificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.SistemaRegistroVerificacao.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRegistroVerificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRegistroVerificacaoApplication.class, args);
+	}
+
+}
