05018c41976653d2d82ebc7134163b57ca20abd3
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-11-18 10:13:58 -0300

    Desenvolvimento UsuarioSeletor e UsuarioSpecification. Verificar se os atributos que são classes enum devem ser escritos dessa forma no specification

--- src/main/java/com/sistemaRegistroVerificacao/model/seletor/UsuarioSeletor.java
+++ src/main/java/com/sistemaRegistroVerificacao/model/seletor/UsuarioSeletor.java
@@ -1,15 +1,48 @@
 package com.sistemaRegistroVerificacao.model.seletor;
 
-public class UsuarioSeletor extends BaseSeletor{
-	
-	private String nome;
+import com.sistemaRegistroVerificacao.model.entity.Afastamento;
+import com.sistemaRegistroVerificacao.model.entity.Cargo;
+import com.sistemaRegistroVerificacao.model.entity.NivelAcesso;
+import com.sistemaRegistroVerificacao.model.entity.StatusUsuario;
 
-	public String getNome() {
-		return nome;
-	}
+import java.time.LocalDate;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-   
+public class UsuarioSeletor extends BaseSeletor {
+
+    private String nome;
+    private String cargo;
+    private String statusUsuario;
+    private LocalDate dataDesligamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatusUsuario() {
+        return statusUsuario;
+    }
+
+    public void setStatusUsuario(String statusUsuario) {
+        this.statusUsuario = statusUsuario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public LocalDate getDataDesligamento() {
+        return dataDesligamento;
+    }
+
+    public void setDataDesligamento(LocalDate dataDesligamento) {
+        this.dataDesligamento = dataDesligamento;
+    }
 }
--- src/main/java/com/sistemaRegistroVerificacao/model/specification/UsuarioSpecification.java
+++ src/main/java/com/sistemaRegistroVerificacao/model/specification/UsuarioSpecification.java
@@ -1,14 +1,43 @@
 package com.sistemaRegistroVerificacao.model.specification;
 
+import com.sistemaRegistroVerificacao.model.entity.Afastamento;
+import com.sistemaRegistroVerificacao.model.seletor.AfastamentoSeletor;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
 import com.sistemaRegistroVerificacao.model.entity.Usuario;
 import com.sistemaRegistroVerificacao.model.seletor.UsuarioSeletor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsuarioSpecification {
 
     public static Specification<Usuario> comFiltros(UsuarioSeletor seletor) {
-        return null;
+        return (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            // WHERE/AND COLUNA OPERADOR VALOR
+            // WHERE      nome   like    %Juliana%
+            if (seletor.getNome() != null) {
+                predicates.add((Predicate) cb.like(cb.lower(root.get("nome")), "%" + seletor.getNome().toLowerCase() + "%"));
+            }
+
+            if (seletor.getDataDesligamento() != null) {
+                predicates.add(cb.equal(root.get("dataDesligamento"), seletor.getDataDesligamento()));
+            }
+
+            if (seletor.getCargo() != null) {
+                predicates.add(cb.equal(root.join("cargo").get("label"),
+                        seletor.getCargo()));
+            }
+
+            if (seletor.getStatusUsuario() != null) {
+                predicates.add(cb.equal(root.join("statusUsuario").get("label"),
+                        seletor.getStatusUsuario()));
+            }
+
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
     }
-    
 }
