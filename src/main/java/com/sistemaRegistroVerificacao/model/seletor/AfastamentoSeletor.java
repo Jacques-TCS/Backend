21fe144140f573a5e5d07308c878324688a24a0a
betossauro <pfvrbeto@gmail.com>
2023-11-18 17:04:39 -0300

    atualização de specification e seletor

--- src/main/java/com/sistemaRegistroVerificacao/model/seletor/AfastamentoSeletor.java
+++ src/main/java/com/sistemaRegistroVerificacao/model/seletor/AfastamentoSeletor.java
@@ -6,8 +6,10 @@ public class AfastamentoSeletor extends BaseSeletor{
 	
 	private String descricao;
 	private String natureza;
-	private LocalDate dataInicio;
-	private LocalDate dataFim;
+	private LocalDate menorDataInicio;
+	private LocalDate maiorDataInicio;
+	private LocalDate menorDataFim;
+	private LocalDate maiorDataFim;
 	
 	public String getDescricao() {
 		return descricao;
@@ -21,17 +23,29 @@ public class AfastamentoSeletor extends BaseSeletor{
 	public void setNatureza(String natureza) {
 		this.natureza = natureza;
 	}
-	public LocalDate getDataInicio() {
-		return dataInicio;
+	public LocalDate getMenorDataInicio() {
+		return menorDataInicio;
 	}
-	public void setDataInicio(LocalDate dataInicio) {
-		this.dataInicio = dataInicio;
+	public void setMenorDataInicio(LocalDate menorDataInicio) {
+		this.menorDataInicio = menorDataInicio;
 	}
-	public LocalDate getDataFim() {
-		return dataFim;
+	public LocalDate getmaiorDataInicio() {
+		return maiorDataInicio;
 	}
-	public void setDataFim(LocalDate dataFim) {
-		this.dataFim = dataFim;
+	public void setMaiorDataInicio(LocalDate maiorDataInicio) {
+		this.maiorDataInicio = maiorDataInicio;
+	}
+	public LocalDate getMenorDataFim() {
+		return menorDataFim;
+	}
+	public void setMenorDataFim(LocalDate menorDataFim) {
+		this.menorDataFim = menorDataFim;
+	}
+	public LocalDate getMaiorDataFim() {
+		return maiorDataFim;
+	}
+	public void setMaiorDataFim(LocalDate maiorDataFim) {
+		this.maiorDataFim = maiorDataFim;
 	}
 	
 	
