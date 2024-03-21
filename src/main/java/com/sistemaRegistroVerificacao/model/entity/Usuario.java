14eb90c52329f60b5104907bfb264ca3c901890b
Rafael Derner <rafael-derner@live.com>
2024-03-21 08:51:49 -0300

    update/autenticacao Ajustes no modelo Usuario para que a autenticação funcione

--- src/main/java/com/sistemaRegistroVerificacao/model/entity/Usuario.java
+++ src/main/java/com/sistemaRegistroVerificacao/model/entity/Usuario.java
@@ -1,7 +1,12 @@
 package com.sistemaRegistroVerificacao.model.entity;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -14,6 +19,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +31,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name = "USUARIO")
-public class Usuario {
+public class Usuario implements UserDetails {
 	public static final String STATUS_ATIVO = "Ativo";
 	public static final String STATUS_INATIVO = "Inativo";
 	public static final String STATUS_AFASTADO = "Afastado";
@@ -51,18 +57,12 @@ public class Usuario {
 	private LocalDate dataNascimento;
 
 	@Column()
-	private String ctps;
+    @NotEmpty(message = "É necessário informar o e-mail")
+	private String email;
 
 	@ManyToOne
 	@JoinColumn(name = "id_cargo", referencedColumnName = "id")
 	private Cargo cargo;
-
-	@Column()
-	private String matricula;
-	
-	@Column()
-    @NotNull(message = "É necessário informar a senha")
-	private String senha;
 
 	@Column(name = "status")
     @NotNull(message = "É necessário informar o status")
@@ -78,4 +78,64 @@ public class Usuario {
 	@Column(name = "data_contratacao")
     @NotNull(message = "É necessário informar a data de contratação")
 	private LocalDate dataContratacao;
+
+	@Column(length = 50)
+    @NotNull(message = "É necessário informar o login")
+    private String username;
+
+	@Column()
+    @NotNull(message = "É necessário informar a senha")
+    private String password;
+
+    @Override
+    /*
+     * Esse método retorna os perfis que o usuário possui.
+     * Idealmente, a classe Usuario deveria ter um atributo do tipo Perfil (ENUM), 
+	 * onde seriam descritos os possíveis perfis que um usuário pode possuir.
+     */
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.cargo.getNome()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    /*
+     * Esse método retorna apenas true porque não existe validação de usuário
+     */
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    /*
+     * Esse método retorna apenas true porque não existe validação de usuário
+     */
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    /*
+     * Esse método retorna apenas true porque não existe validação de usuário
+     */
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    /*
+     * Esse método retorna apenas true porque não existe validação de usuário ativo ou inativo
+     */
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/sistemaRegistroVerificacao/model/repository/UsuarioRepository.java
+++ src/main/java/com/sistemaRegistroVerificacao/model/repository/UsuarioRepository.java
@@ -2,8 +2,14 @@ package com.sistemaRegistroVerificacao.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.sistemaRegistroVerificacao.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>, JpaSpecificationExecutor<Usuario> {
+    
+    /*
+     * Esse método é utilizado pelo Spring Security para a autenticação do usuário
+     */
+    UserDetails findByUsername(String username);
 }
